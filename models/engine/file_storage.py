bc19e712238d32a434428cc92a793ad366ab598b
Chymezy <chymezy332@gmail.com>
2024-03-20 22:30:03 +0000

    adding updates

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,80 +1,69 @@
 #!/usr/bin/python3
-'''
-    Define class FileStorage
-'''
+"""This module defines a class to manage file storage for hbnb clone"""
+
+
 import json
-import models
+from models.base_model import BaseModel
+from models.amenity import Amenity
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.state import State
+from models.user import User
 
 
 class FileStorage:
-    '''
-        Serializes instances to JSON file and deserializes to JSON file.
-    '''
-    __file_path = "file.json"
+    """This class manages storage of hbnb models in JSON format"""
+    __file_path = 'file.json'
     __objects = {}
 
     def all(self, cls=None):
-        '''
-            Return the dictionary
-        '''
-        new_dict = {}
+        """Returns a dictionary of models currently in storage"""
         if cls is None:
             return self.__objects
-
-        if cls != "":
-            for k, v in self.__objects.items():
-                if cls == k.split(".")[0]:
-                    new_dict[k] = v
-            return new_dict
         else:
-            return self.__objects
+            if isinstance(cls, str):
+                cls = eval(cls)
+            clsDict = {}
+            for key, value in self.__objects.items():
+                if isinstance(value, cls):
+                    clsDict[key] = value
+            return clsDict
 
     def new(self, obj):
-        '''
-            Set in __objects the obj with key <obj class name>.id
-            Aguments:
-                obj : An instance object.
-        '''
-        key = str(obj.__class__.__name__) + "." + str(obj.id)
-        value_dict = obj
-        FileStorage.__objects[key] = value_dict
+        """Adds new object to storage dictionary"""
+        key = "{}.{}".format(type(obj).__name__, obj.id)
+        self.__objects[key] = obj
 
     def save(self):
-        '''
-            Serializes __objects attribute to JSON file.
-        '''
-        objects_dict = {}
-        for key, val in FileStorage.__objects.items():
-            objects_dict[key] = val.to_dict()
-
-        with open(FileStorage.__file_path, mode='w', encoding="UTF8") as fd:
-            json.dump(objects_dict, fd)
+        """Saves storage dictionary to file"""
+        with open(self.__file_path, 'w') as f:
+            tmp = {}
+            for key, value in self.__objects.items():
+                tmp[key] = value.to_dict()
+            json.dump(tmp, f)
 
     def reload(self):
-        '''
-            Deserializes the JSON file to __objects.
-        '''
+        """Loads storage dictionary from file"""
         try:
-            with open(FileStorage.__file_path, encoding="UTF8") as fd:
-                FileStorage.__objects = json.load(fd)
-            for key, val in FileStorage.__objects.items():
-                class_name = val["__class__"]
-                class_name = models.classes[class_name]
-                FileStorage.__objects[key] = class_name(**val)
+            with open(self.__file_path, "r") as f:
+                od = json.load(f)
+                for obj in od.values():
+                    clsName = obj["__class__"]
+                    del obj["__class__"]
+                    self.new(eval(clsName)(**obj))
         except FileNotFoundError:
             pass
 
     def delete(self, obj=None):
-        '''
-        Deletes an obj
-        '''
-        if obj is not None:
-            key = str(obj.__class__.__name__) + "." + str(obj.id)
-            FileStorage.__objects.pop(key, None)
-            self.save()
+        """Delete a given object from __objects, if it exists"""
+        if obj:
+            del self.__objects["{}.{}".format(type(obj).__name__, obj.id)]
 
     def close(self):
-        '''
-        Deserialize JSON file to objects
-        '''
+        """Call the reload method"""
         self.reload()
+
+    def clear(self):
+        """Clear all objects from storage"""
+        self.__objects.clear()
--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -1,99 +1,97 @@
 #!/usr/bin/python3
-""" """
-from models.base_model import BaseModel
+"""Defines unittests for models/base_model.py"""
+
+
 import unittest
-import datetime
-from uuid import UUID
-import json
 import os
+from models.base_model import BaseModel
+from datetime import datetime
+import models
 
 
-class test_basemodel(unittest.TestCase):
-    """ """
-
-    def __init__(self, *args, **kwargs):
-        """ """
-        super().__init__(*args, **kwargs)
-        self.name = 'BaseModel'
-        self.value = BaseModel
+class TestBaseModel(unittest.TestCase):
+    """Test cases for the BaseModel class"""
 
     def setUp(self):
-        """ """
-        pass
-
-    def tearDown(self):
+        """Set up test environment"""
         try:
-            os.remove('file.json')
-        except:
+            os.remove("file.json")
+        except FileNotFoundError:
             pass
 
-    def test_default(self):
-        """ """
-        i = self.value()
-        self.assertEqual(type(i), self.value)
+    def tearDown(self):
+        """Clean up test environment"""
+        try:
+            os.remove("file.json")
+        except FileNotFoundError:
+            pass
 
-    def test_kwargs(self):
-        """ """
-        i = self.value()
-        copy = i.to_dict()
-        new = BaseModel(**copy)
-        self.assertFalse(new is i)
+    def test_instance_creation(self):
+        """Test creation of BaseModel instance"""
+        model = BaseModel()
+        self.assertIsInstance(model, BaseModel)
 
-    def test_kwargs_int(self):
-        """ """
-        i = self.value()
-        copy = i.to_dict()
-        copy.update({1: 2})
-        with self.assertRaises(TypeError):
-            new = BaseModel(**copy)
+    def test_id_is_string(self):
+        """Test id attribute is a string"""
+        model = BaseModel()
+        self.assertIsInstance(model.id, str)
 
-    def test_save(self):
-        """ Testing save """
-        i = self.value()
-        i.save()
-        key = self.name + "." + i.id
-        with open('file.json', 'r') as f:
-            j = json.load(f)
-            self.assertEqual(j[key], i.to_dict())
+    def test_created_at_is_datetime(self):
+        """Test created_at attribute is a datetime"""
+        model = BaseModel()
+        self.assertIsInstance(model.created_at, datetime)
 
-    def test_str(self):
-        """ """
-        i = self.value()
-        self.assertEqual(str(i), '[{}] ({}) {}'.format(self.name, i.id,
-                         i.__dict__))
+    def test_updated_at_is_datetime(self):
+        """Test updated_at attribute is a datetime"""
+        model = BaseModel()
+        self.assertIsInstance(model.updated_at, datetime)
 
-    def test_todict(self):
-        """ """
-        i = self.value()
-        n = i.to_dict()
-        self.assertEqual(i.to_dict(), n)
+    def test_save_method(self):
+        """Test save method updates updated_at"""
+        model = BaseModel()
+        original_updated_at = model.updated_at
+        model.save()
+        self.assertNotEqual(original_updated_at, model.updated_at)
 
-    def test_kwargs_none(self):
-        """ """
-        n = {None: None}
-        with self.assertRaises(TypeError):
-            new = self.value(**n)
+    def test_to_dict_method(self):
+        """Test to_dict method returns dictionary"""
+        model = BaseModel()
+        model_dict = model.to_dict()
+        self.assertIsInstance(model_dict, dict)
 
-    def test_kwargs_one(self):
-        """ """
-        n = {'Name': 'test'}
-        with self.assertRaises(KeyError):
-            new = self.value(**n)
+    def test_to_dict_contains_classname(self):
+        """Test to_dict method contains __class__ key with class name"""
+        model = BaseModel()
+        model_dict = model.to_dict()
+        self.assertIn("__class__", model_dict)
+        self.assertEqual(model_dict["__class__"], "BaseModel")
 
-    def test_id(self):
-        """ """
-        new = self.value()
-        self.assertEqual(type(new.id), str)
+    def test_to_dict_datetime_format(self):
+        """Test to_dict method returns correct datetime format"""
+        model = BaseModel()
+        model_dict = model.to_dict()
+        self.assertEqual(model.created_at.isoformat(),
+                         model_dict["created_at"])
+        self.assertEqual(model.updated_at.isoformat(),
+                         model_dict["updated_at"])
 
-    def test_created_at(self):
-        """ """
-        new = self.value()
-        self.assertEqual(type(new.created_at), datetime.datetime)
+    def test_to_dict_method_excludes_sa_instance_state(self):
+        """Test to_dict method excludes _sa_instance_state key"""
+        model = BaseModel()
+        model_dict = model.to_dict()
+        self.assertNotIn("_sa_instance_state", model_dict)
 
-    def test_updated_at(self):
-        """ """
-        new = self.value()
-        self.assertEqual(type(new.updated_at), datetime.datetime)
-        n = new.to_dict()
-        new = BaseModel(**n)
-        self.assertFalse(new.created_at == new.updated_at)
+    def test_to_dict_method_includes_custom_attributes(self):
+        """Test to_dict method includes custom attributes"""
+        model = BaseModel()
+        model.name = "Test"
+        model.number = 123
+        model_dict = model.to_dict()
+        self.assertIn("name", model_dict)
+        self.assertIn("number", model_dict)
+        self.assertEqual(model_dict["name"], "Test")
+        self.assertEqual(model_dict["number"], 123)
+
+
+if __name__ == '__main__':
+    unittest.main()
